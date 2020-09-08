9c11b0c9270754bbeb8e103b0db7c48d71694f1b
sashioma <friandise06@gmail.com>
2020-09-08 14:28:01 +0300

    Добавляет файлы для отслеживания

--- gulpfile.js
+++ gulpfile.js
@@ -150,7 +150,8 @@ exports.build = build;
 
 const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
-  gulp.watch("source/*.html").on("change", sync.reload);
+  gulp.watch("source/*.html", gulp.series("html"));
+  gulp.watch("source/js/*.js", gulp.series("scripts"));
 }
 
 exports.default = gulp.series(
